72b227b3f6f6562fdf8354937115514e1b812856
Jimmy618 <wong124816@gmail.com>
2021-10-20 19:54:36 +0800

    categorical analysis

--- staticalAnalysis/pandaTesting.py
+++ staticalAnalysis/pandaTesting.py
@@ -7,11 +7,19 @@ to_csv = True
 dataPath = r'../data/'
 resultPath = r'../result/'
 
+# since not every SMART is reported in failureData, 
+# only selection of SMART data is used in analysis.
+colList = ['r_5', 'n_5', 'r_183', 'n_183', 'r_184', 'n_184', 'r_187', 'n_187',
+           'r_195', 'n_195', 'r_197', 'n_197', 'r_199', 'n_199', 
+           # to be added
+           # 'r_program', 'n_program', 'r_erase', 'n_erase', 'n_blocks', 'n_wearout', 
+           'r_241', 'n_241','r_242', 'n_242', 'r_9', 'n_9', 
+           'r_12', 'n_12', 'r_174', 'n_174', 'n_175']
 
-def cateComposition(columnName, locationData, failureData):
-    # this function intent to analysis composition of categorical data
+def categoricalComposition(columnName, locationData, failureData):
+    # this function intent to analyse composition of categorical data
 
-    print("processing", columnName, "composition")
+    print("Processing", columnName, "composition")
     
     totalresult = locationData.groupby(columnName).size().to_frame('size')
     totalcount = locationData[columnName].count()
@@ -31,62 +39,92 @@ def cateComposition(columnName, locationData, failureData):
         # result.plot.pie(y='sizePercentage')
         plt.show()
 
-
-def numComposition(colList, smartData, failureData):
-    print("processing SMART composition")
-    
-    smart_df = pd.DataFrame(failureData, columns=colList)
-    smart_quantile = smart_df.quantile(q=[0.25, 0.5, 0.75]).T.reset_index()
-    smart_quantile = smart_quantile.rename(columns={0.25:'0.25 quantile',
-    0.5:'0.5 quantile',
-    0.75:'0.75 quantile'})
-    smart_mean = smart_df.mean().reset_index()
-    smart_mean = smart_mean.rename(columns={0:'mean'})
-    smart_std = smart_df.std().reset_index()
-    smart_std = smart_std.rename(columns={0:'std'})
+def extractSMARTHelper(df, postfix):
+    smart_df = pd.DataFrame(df, columns=colList)
+    smart_quantile = smart_df.quantile(q=[0.25, 0.5, 0.75]).T
+    smart_quantile = smart_quantile.rename(columns={0.25:'0.25 quantile' + postfix,
+    0.5:'0.5 quantile' + postfix,
+    0.75:'0.75 quantile' + postfix})
+    smart_mean = smart_df.mean()
+    smart_mean = smart_mean.rename('mean' + postfix)
+    smart_std = smart_df.std()
+    smart_std = smart_std.rename('std' + postfix)
     
     dataframes = [smart_quantile, smart_mean, smart_std]
 
-    result = reduce(lambda  left,right: pd.merge(left,right,on=['index']), dataframes)
+    return pd.concat(dataframes, axis=1)
 
-    working_smart_df = pd.DataFrame(smartData, columns=colList)
-    working_smart_quantile = working_smart_df.quantile(q=[0.25, 0.5, 0.75]).T.reset_index()
-    working_smart_quantile = working_smart_quantile.rename(columns={0.25:'0.25 quantile (functional)',
-    0.5:'0.5 quantile (functional)',
-    0.75:'0.75 quantile (functional)'})
-    working_smart_mean = working_smart_df.mean().reset_index()
-    working_smart_mean = working_smart_mean.rename(columns={0:'mean (functional)'})
-    working_smart_std = working_smart_df.std().reset_index()
-    working_smart_std = working_smart_std.rename(columns={0:'std (functional)'})
+    # old code
+    #return reduce(lambda  left,right: pd.merge(left,right,on=['index']), dataframes)
+
+def numComposition(smartData, failureData):
+    # this function intent to analyse composition of numerial data
+    print("Processing SMART composition")
     
-    working_dataframes = [working_smart_quantile, working_smart_mean, working_smart_std]
+    result = extractSMARTHelper(failureData, '')
+    working_result = extractSMARTHelper(smartData, ' (functional)')
 
-    working_result = reduce(lambda  left,right: pd.merge(left,right,on=['index']), working_dataframes)
-
-    output = result.merge(working_result, on='index')
+    #output = result.merge(working_result, on='index')
+    output = pd.concat([result, working_result], axis=1)
 
     if to_csv:
         output.to_csv(resultPath + 'composition' + '_' + 'SMART' + '.csv')
 
+def categoricalSMART(columnName, smartData, failureData):
+    # this function intent to analyse SMART of each instance of the categorical data
 
-# incase the program is running in conda, check for '__main__'
+    print("Processing", columnName + "'s", "SMART")
+
+    smart_df = smartData.groupby(columnName)
+    smart_fail_df = failureData.groupby(columnName)
+    temList = []
+    list = []
+    # assume both smartData and failureData share the same 'columnName' categorical variable
+    for key, item in smart_df:
+        # print(key)
+        # print(smart_df.get_group(key))
+        # print(smart_fail_df.get_group(key))
+        fail_result = extractSMARTHelper(smart_df.get_group(key), '')
+        result = extractSMARTHelper(smart_df.get_group(key), ' (functional)')
+        #output = fail_result.merge(result, on='index')
+        output = pd.concat([fail_result, result], axis=1)
+        #print(output)
+        temList.append(output)
+        list.append(key)
+        print(len(temList))
+    print(temList)
+    print(list)
+    
+    df = pd.concat(temList, keys=list)
+    print(df)
+    
+    if to_csv:
+        df.to_csv(resultPath + columnName + '\'s ' + 'group ' + 'SMART' + '.csv')
+    
+    # tem = pd.DataFrame(df.get_group('A1'), columns=colList)
+    # result = extractsmarthelper(collist, smartdata, '')
+    # print(result)
+    # result = extractsmarthelper(collist, df.get_group('a1'), '')
+    # print(result)
+    # result = extractsmarthelper(collist, df.get_group('a2'), '')
+    # print(result)
+
+
+# In case the program is running in conda, check for '__main__'
 # to ensure it run.
 if __name__ == '__main__':
-    print("preparing csv")
+    print("Preparing csv")
     locationData = pd.read_csv(dataPath + 'location_info_of_ssd.csv')
     smartData = pd.read_csv(dataPath + '20191231.csv')
     failureData = pd.read_csv(dataPath + 'ssd_failure_tag.csv')
 
-    cateComposition('model', locationData, failureData)
-    cateComposition('app', locationData, failureData)
+    categoricalComposition('model', locationData, failureData)
+    categoricalComposition('app', locationData, failureData)
 
-    colList = ['r_5', 'n_5', 'r_183', 'n_183', 'r_184', 'n_184', 'r_187', 'n_187',
-               'r_195', 'n_195', 'r_197', 'n_197', 'r_199', 'n_199', 
-               # 'r_program', 'n_program', 'r_erase', 'n_erase', 'n_blocks', 'n_wearout', 
-               'r_241', 'n_241','r_242', 'n_242', 'r_9', 'n_9', 
-               'r_12', 'n_12', 'r_174', 'n_174', 'n_175']
+    numComposition(smartData, failureData)
 
-    numComposition(colList, smartData, failureData)
+    # extract relation between differenct model and application
+    categoricalSMART('model', smartData, failureData)
 
     # print(result)
 
