9ba71ebf97a7fa3e44b3ef1acc3fa9f851eefcc5
jimmy618 <wong124816@gmail.com>
2022-04-01 15:55:38 +0800

    baseline ML

--- machineLearning/ml.py
+++ machineLearning/ml.py
@@ -1,34 +1,41 @@
+import sys
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torch import nn
-from torch.optim import SGD
 # from torchvision import transforms, utils
-
-# prepate dataset:
-# forget locaiton.csv, just use ssd_failure_tag.csv as failed and 20191231.csv as functional
-# shape two into unified.
-# combine some SMART in functional data
-# combine two .csv in one
-# smartData.join(locationData.set_index('disk_id').drop('model', axis=1), on='disk_id')
 
 # ML
 
 # incase the program is running in conda, check for '__main__'
 # to ensure it run.
 if __name__ == '__main__':
-    torch.autograd.set_detect_anomaly(True)
-    batch_size = 32
+    try:
+        trainModel = sys.argv[1] == 'train'
+    except IndexError:
+        trainModel = False
+
+    # torch.autograd.set_detect_anomaly(True)
+    train_batch_size = 1024
 
     print("Preparing dataset")
     dataPath = r'../data/'
     smartData = pd.read_csv(dataPath + '20191231.csv')
     failureData = pd.read_csv(dataPath + 'ssd_failure_tag.csv')
+    # location data is not used in ML because the location information (rank_id, disk_id, ...)
+    # are not provided in Healthy SSDs data (smartData)
+    # Although disk_id is indeed provided, serveral entries in 20191231.csv share the same disk_id,
+    # presumably the same disk location is replaced serveral time or disk_id is resused in different location
+    # so disk_id is not a key to join and find the locaiton
+    # locationData = pd.read_csv(dataPath + 'location_info_of_ssd.csv')
 
-    # 'n_blocks' 'n_170', 'n_180'
-    # 'r_program', 'n_program' 'n_171', 'n_181'
-    # 'r_erase', 'n_erase' 'n_172', 'n_182'
-    # 'n_wearout', 'n_173' 'n_177', 'n_233'
+    # since only portion of SMART data are provided in ssd_failure_tag.csv,
+    # only correponding part of information in 20191231.csv are used
+    # The provide SMART, as specificied in ssd_open_data READMO.md,
+    # 'n_blocks': 'n_170', 'n_180'
+    # 'r_program': 'n_program' 'n_171', 'n_181'
+    # 'r_erase' 'n_erase': 'n_172', 'n_182'
+    # 'n_wearout', 'n_173': 'n_177', 'n_233'
     colList = ['model', 'r_5', 'n_5', 'r_183', 'n_183', 'r_184', 'n_184', 'r_187', 'n_187',
                'r_195', 'n_195', 'r_197', 'n_197', 'r_199', 'n_199',
                'r_program', 'n_program', 'r_erase', 'n_erase', 'n_blocks', 'n_wearout',
@@ -44,6 +51,8 @@ if __name__ == '__main__':
                         'r_241', 'n_241', 'r_242', 'n_242', 'r_9', 'n_9',
                         'r_12', 'n_12', 'r_174', 'n_174', 'n_175']
 
+    # prepare health data
+    # convert healthy data, use max approach
     datah = pd.DataFrame(columns=[colList])
     for colName in assignHealthyList:
         datah[colName] = smartData[colName]
@@ -55,35 +64,41 @@ if __name__ == '__main__':
     datah['n_wearout'] = smartData[['n_173', 'n_177', 'n_233']].max(axis=1)
     datah['health'] = 1
 
+    # prepare failure data
     dataf = pd.DataFrame(columns=[colList])
     for colName in assignFaultyList:
         dataf[colName] = failureData[colName]
     dataf['health'] = 0
 
+    # since number of health data is much larger than failure data 706182 > 18387,
+    # almost all example feeded to the machine are health data (health label = 1)
+    # it will predict every example to be health and still have high accuracy
+    # perform undersampling to balance number of data
+    datah = datah.sample(n=len(dataf.index))
+
+    # join both data in one dataset
     data = pd.concat([datah, dataf])  # .reset_index(drop=True)
     data['model'] = data['model'].replace(
         ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'B1', 'B2', 'B3', 'C1', 'C2'],
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    # data = data.astype('float')
-    data = data.fillna(-1)
-    data = data.astype(float)
+    # treat nan as value -1
+    data = data.fillna(-1).astype(float).reset_index(drop=True)
 
-    # train, test = random_split(data, [3, len(data.index)-3])
-    train = data.sample(frac=0.8, random_state=200).reset_index(drop=True)
-    test = data.drop(train.index).sample(frac=1.0).reset_index(drop=True)
+    # data_set = ssdDataset(data)
+    # data_loader = DataLoader(data_set, batch_size=batch_size, shuffle=False)
+    # inputs, classes = next(iter(data_loader))
 
-    # csvDataset is modified from pytorch data loading tutorial
+    # ssdDataset is modified from pytorch data loading tutorial
     # source: https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
 
     class ssdDataset(Dataset):
-        """testing csv dataset."""
-
         def __init__(self, dataFrame, transform=None):
             """
             Args:
             dataFrame (panda dataFrame): source of ssd data, prepared.
             transform (callable, optional): Optional transform to be applied on a sample.
             """
+            # since most pre-processing are done before hand, not much changed are needed
             self.dataFrame = dataFrame
             self.transform = transform
 
@@ -93,7 +108,8 @@ if __name__ == '__main__':
         def __getitem__(self, idx):
             if torch.is_tensor(idx):
                 idx = idx.tolist()
-            #sample = self.dataFrame.loc[idx].drop(columns='health', level=0)
+            # sample = self.dataFrame.loc[idx].drop(columns='health', level=0)
+            # remove the label 'health' from the example
             sample = self.dataFrame.loc[idx][colList[:-1]]
             sample = torch.tensor(sample)
             label = self.dataFrame.loc[idx]['health']
@@ -103,6 +119,7 @@ if __name__ == '__main__':
 
             return sample, label
 
+    # define the network
     # https://github.com/christianversloot/machine-learning-articles/blob/main/creating-a-multilayer-perceptron-with-pytorch-and-lightning.md
     class MLP(nn.Module):
         '''
@@ -120,58 +137,60 @@ if __name__ == '__main__':
               nn.Linear(32, 10),
               nn.ReLU(),
               nn.Linear(10, 1),
-              nn.Sigmoid()
+              nn.Sigmoid()  # sigmoid function to convert result to [0,1]
             )
 
         def forward(self, x):
             '''Forward pass'''
             return self.layers(x)
 
-    # data_set = ssdDataset(data)
-    # data_loader = DataLoader(data_set, batch_size=batch_size, shuffle=False)
-    # inputs, classes = next(iter(data_loader))
-    train_set = ssdDataset(train)
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
-    test_set = ssdDataset(test)
-    test_loader = DataLoader(test_set, batch_size=1024, shuffle=True)
-    # define the network
-    model = MLP()
-    # model.double()
+    if trainModel:
+        print("Training Neural Network")
+        train = data.sample(frac=0.8).reset_index(drop=True)
+        train_set = ssdDataset(train)
+        train_loader = DataLoader(train_set, batch_size=train_batch_size, shuffle=True)
+        model = MLP()
 
-    loss_function = nn.BCELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+        loss_function = nn.BCELoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-    # Run the training loop
-    for epoch in range(0, 1):  # 5 epochs at maximum
-        # Print epoch
-        print(f'Starting epoch {epoch+1}')
-        # Set current loss value
-        current_loss = 0.0
-        # Iterate over the DataLoader for training data
-        for i, data in enumerate(train_loader, 0):
-            # Get inputs
-            inputs, targets = data
+        for epoch in range(0, 1):
+            print(f'Starting epoch {epoch+1}')
+            current_loss = 0.0
+            for i, data in enumerate(train_loader, 0):
+                inputs, targets = data
+                optimizer.zero_grad()
+                outputs = model(inputs.float())
+                loss = loss_function(outputs, targets.unsqueeze(1).float())
+                loss.backward()
+                optimizer.step()
+                current_loss += loss.item()
+                if i % 10 == 9:
+                    print('Loss after mini-batch %5d: %.3f' % (i + 1, current_loss / 500))
+                    current_loss = 0.0
 
-            # Zero the gradients
-            optimizer.zero_grad()
+        print('Training process has finished.')
+        torch.save(model.state_dict(), './model.pt')
 
-            # Perform forward pass
-            outputs = model(inputs.float())
+    if not trainModel:
+        print('Calculating Accuracy')
 
-            # Compute loss
-            loss = loss_function(outputs, targets.unsqueeze(1).float())
+        test = data.sample(frac=0.1).reset_index(drop=True)
+        test_set = ssdDataset(test)
+        test_loader = DataLoader(test_set, batch_size=1024, shuffle=True)
 
-            # Perform backward pass
-            loss.backward()
+        model = MLP()
+        model.load_state_dict(torch.load('./model.pt'))
 
-            # Perform optimization
-            optimizer.step()
+        correct_num = 0
+        sample_num = len(test.index)
+        model.eval()
 
-            # Print statistics
-            current_loss += loss.item()
-            if i % 500 == 499:
-                print('Loss after mini-batch %5d: %.3f' % (i + 1, current_loss / 500))
-                current_loss = 0.0
+        with torch.no_grad():
+            for i, data in enumerate(test_loader):
+                inputs, targets = data
+                outputs = model(inputs.float())
+                correct_num += (outputs.round() == targets.unsqueeze(1)).float().sum().item()
 
-    # Process is complete.
-    print('Training process has finished.')
+            print(f'Correct / Total : {correct_num} / {sample_num}')
+            print(f'Percentage: {float(correct_num)/float(sample_num)*100: .2f} %')
